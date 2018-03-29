48ecb25be5e5b72313258e69d8786ce25f29a419
Jimmy McNamara and Shirley Dennis <shirley@>
2018-03-29 10:38:49 -0700

    Add logic to js, final commit

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,15 @@ $(document).ready(function() {
     event.preventDefault();
     var year = parseInt ($("input#year").val());
     var result =leapYear(year);
-    $("result").text(result);
+    $("#result").text(result);
   });
-});   
+});
+
+var leapYear  = function(year) {
+
+  if (year % 4 === 0 && year % 100 !== 0 || year % 400 === 0) {
+  return true;
+} else {
+  return false;
+}
+};
